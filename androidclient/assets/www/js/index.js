602485554ee7f6c29c7c34619fc3b51375927189
lnanek <lnanek@gmail.com>
2013-01-09 17:48:16 -0800

    Next image.

--- androidclient/assets/www/js/index.js
+++ androidclient/assets/www/js/index.js
@@ -2,6 +2,73 @@
 var cachedData;
 var currentItem = 0;
 
+function onShake() {
+	alert("shaken");
+}
+
+var shake = (function () {
+	var shake = {},
+		watchId = null,
+		options = { frequency: 300 },
+		previousAcceleration = { x: null, y: null, z: null },
+		shakeCallBack = onShake;
+	
+	// Start watching the accelerometer for a shake gesture
+	shake.startWatch = function (onShake) {
+		shakeCallBack = onShake;
+		watchId = navigator.accelerometer.watchAcceleration(getAccelerationSnapshot, handleError, options);
+	};
+	
+	// Stop watching the accelerometer for a shake gesture
+	shake.stopWatch = function () {
+		if (watchId !== null) {
+			navigator.accelerometer.clearWatch(watchId);
+			watchId = null;
+		}
+	};
+	
+	// Gets the current acceleration snapshot from the last accelerometer watch
+	function getAccelerationSnapshot() {
+		navigator.accelerometer.getCurrentAcceleration(assessCurrentAcceleration, handleError);
+	}
+	
+	// Assess the current acceleration parameters to determine a shake
+	function assessCurrentAcceleration(acceleration) {
+		var accelerationChange = {};
+		if (previousAcceleration.x !== null) {
+			accelerationChange.x = Math.abs(previousAcceleration.x, acceleration.x);
+			accelerationChange.y = Math.abs(previousAcceleration.y, acceleration.y);
+			accelerationChange.z = Math.abs(previousAcceleration.z, acceleration.z);
+		}
+		if (accelerationChange.x + accelerationChange.y + accelerationChange.z > 30) {
+			// Shake detected
+			if (typeof (shakeCallBack) === "function") {
+				shakeCallBack();
+			}
+			shake.stopWatch();
+			setTimeout(shake.startWatch, 1000);
+			previousAcceleration = { 
+				x: null, 
+				y: null, 
+				z: null
+			}
+		} else {
+			previousAcceleration = {
+				x: acceleration.x,
+				y: acceleration.y,
+				z: acceleration.z
+			}
+		}
+	}
+
+	// Handle errors here
+	function handleError() {
+	}
+	
+	return shake;
+})();
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -56,10 +123,15 @@ var app = {
 		        	
 		        		//document.write('<img src="' + data.image + '" />');
 		        	} else {
-		        		document.write(data.name);
+						$('#messageBox').html("No more. Keep going!");
 		        	}
+		        	
+		        	$(".grid").hide();
+		        	
 		        
 		        },'html'); 
+		        
+		        shake.startWatch();
 		
 		};
 
