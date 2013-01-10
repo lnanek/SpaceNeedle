3ff14f051c3b123c10fb5235a210683d4c5e725c
lnanek <lnanek@gmail.com>
2013-01-09 17:27:05 -0800

    See next image.

--- androidclient/assets/www/js/index.js
+++ androidclient/assets/www/js/index.js
@@ -1,21 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+var cachedData;
+var currentItem = 0;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -57,13 +43,16 @@ var app = {
 			
         	console.log('Received coordinates: ' + ll);
 		
-		       $.getJSON('http://server.neatocode.com/spaceneedle/explore.php',function(data,status) {
-        			console.log('Received data: ' + data.image);
-		        
+		       $.getJSON('http://server.neatocode.com/spaceneedle/explore-list.php',function(data,status) {
+        			console.log('Received data: ' + data);
+
+
+			        cachedData = data;
+
 		        	//alert(data);
 		        
-		        	if ( data.image ) {
-		        		$('#topImage').attr("src", data.image);
+		        	if ( data && data[0].image ) {
+		        		$('#topImage').attr("src", data[0].image);
 		        	
 		        		//document.write('<img src="' + data.image + '" />');
 		        	} else {
@@ -86,3 +75,17 @@ var app = {
         
     }
 };
+
+function nextButtonPressed() {
+	$('#messageBox').html("Loading next...");
+	
+	currentItem++;
+	
+	if ( cachedData && cachedData[currentItem].image ) {
+		$('#topImage').attr("src", cachedData[currentItem].image);
+	} else {
+		$('#messageBox').html("No more. Keep going!");
+	}	
+	
+	
+}
